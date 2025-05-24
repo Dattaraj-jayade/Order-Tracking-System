ddf9ff7e13025834a304ed7db50c13e269a9b9a0
DATTARAJ JAYADE <60321075+Dattaraj-jayade@users.noreply.github.com>
2025-05-24 15:28:56 +0530

    Add files via upload

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors'
+import clientRoutes from './routes/clientRoute.js'
+import orderRoutes from './routes/orderRoute.js'
+import order_itemsRoutes from './routes/order_itemRoute.js'
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', clientRoutes);
+app.use('/api', orderRoutes);
+app.use('/api',order_itemsRoutes);
+app.listen(port, () => {
+    console.log("listening on port 3000")
+});
