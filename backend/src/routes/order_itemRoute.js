13a6ac88c8b7881ca8b4bad85b992b05972ba502
DATTARAJ JAYADE <60321075+Dattaraj-jayade@users.noreply.github.com>
2025-05-24 15:27:50 +0530

    Add files via upload

---
+++ backend/src/routes/order_itemRoute.js
@@ -0,0 +1,14 @@
+
+import express from 'express';
+import * as order_itemsController from '../controllers/order_itemControllers.js'
+
+const router = express.Router();
+
+router.get('/order_items', order_itemsController.getorder_items);
+router.post('/order_items', order_itemsController.createOrder_items);
+router.put('/order_items/:id', order_itemsController.updateOrder_items);
+router.delete('/order_items/:id', order_itemsController.deleteOrder_items);
+
+
+
+export default router;
